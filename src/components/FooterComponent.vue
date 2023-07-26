d29bdfee743ae40fe481ca4f27b7dea04a72fc31
EnricoValente <enricovalente42@gmail.com>
2023-07-26 10:21:34 +0200

    commit section 1 main

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -11,7 +11,7 @@
 
 <template>
     <footer>
-        Footer
+        
     </footer>
 </template>
 
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -4,15 +4,48 @@
     data() {
         return {
             store,
+            portCards: [
+                {
+                    src: 'h-2-port-img-1.jpg'
+                },
+                {
+                    src: 'h-2-port-img-2.jpg'
+                },
+                {
+                    src: 'h-2-port-img-3.jpg'
+                },
+                {
+                    src: 'h-2-port-img-4.jpg'
+                },
+                {
+                    src: 'h-2-port-img-5.jpg'
+                },
+                {
+                    src: 'h-2-port-img-6.jpg'
+                },
+            ]
+        }
+    },
+    methods: {
+        getImagePath: function(src) {
+            return new URL(`../assets/img/${src}` , import.meta.url).href;
+            
         }
     }
-    }
+}
 </script>
 
 
 <template>
     <main>
-       <!-- {{ store.images }} -->
+        <section>
+            <div class="row">
+              <div class="col-4 g-0" v-for="(singleCard, i) in portCards" :key="i">
+                <img :src="getImagePath(singleCard.src)" alt="">
+              </div>
+            </div>
+        </section>
+            
     </main>
 </template>
 
